0b356d7cbef15fcb055b3c46a744d0556864fceb
Nick Coding <Nick Coding@VELVETPC>
2021-10-20 18:54:56 -0600

    Add project files.

---
+++ OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,7 @@
+﻿namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None, Mexican, Italian,Indian
+    }
+}
---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Hometown Food", Location = "Maryland", Cuisine = CuisineType.None},
+                new Restaurant {Id = 3, Name = "Good Food", Location = "Maryland", Cuisine = CuisineType.Indian}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
